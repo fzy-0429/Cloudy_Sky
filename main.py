25e70ee8951039dda2cb0ff110095c63af70d16b
daf201 <daf201@blink-in.com>
2024-04-13 19:55:19 -0500

    test ver

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import simple_socket
+from Cloudy_Sky.src import simple_socket_server
 from os import getuid
 from src.tools import log
 from src.Block_Chain import block, chain
@@ -8,7 +8,7 @@ from time import sleep
 if getuid() != 0:
     print("sudo required")
     exit(0)
-s = simple_socket.simple_socket_server(1, interface=True)
+s = simple_socket_server.simple_socket_server(1, interface=True)
 
 while 1:
     s.UDP_broadcast(b"broadcast", 50)
