ad60b33b5fd232b9b2e600b02a6f81c0cba44cb8
daf201 <daf201@blink-in.com>
2024-04-13 20:09:00 -0500

    github blowup ver

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Cloudy_Sky.src import simple_socket_server
+from src import simple_socket_server
 from os import getuid
 from src.tools import log
 from src.Block_Chain import block, chain
@@ -11,5 +11,5 @@ if getuid() != 0:
 s = simple_socket_server.simple_socket_server(1, interface=True)
 
 while 1:
-    s.UDP_broadcast(b"broadcast", 50)
+    s.UDP_broadcast(b"broadcast", 51)
     sleep(5)
