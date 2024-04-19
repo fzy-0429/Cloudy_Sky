6ad8662dda9dc24cb1c27f2a0d1e11cf2f123648
daf201 <daf201@blink-in.com>
2024-04-19 10:27:59 -0500

    test ver for client

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src import simple_socket_server
 from src import sky
 from os import getuid
 from src.tools import log
-from src.sky import beacon
+from src.sky import sky
 from time import sleep
 from src.bc import Transaction
 from pickle import dumps
@@ -10,9 +10,4 @@ from pickle import dumps
 if getuid() != 0:
     print("sudo required")
     exit(0)
-s = sky.beacon()
-s.get_block_chain().add_transaction(Transaction("ALICE", "BOB", 10, 0))
-
-while 1:
-    sleep(1)
-    s.broadcast()
+s = sky()
